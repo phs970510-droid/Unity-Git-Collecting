c17f1c152bb7cd006803cfc55bb9a149c9bebcb4
phs970510-droid <phs970510@gmail.com>
2025-11-13 18:19:42 +0900

    new1

---
+++ Assets/02. Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("점프")]
+    [SerializeField] private float jumpForce = 7.0f;
+
+    [Header("바닥 체크")]
+    public Transform groundCheck;                               //발밑 위치를 나타내는 트랜스폼
+    [SerializeField] private float groundCheckRadius = 0.3f;    //감지용 반지름
+    [SerializeField] private LayerMask groundLayer;             //레이어 설정
+
+    [Header("시야")]
+    [SerializeField] private Transform yawRoot;
+    [SerializeField] private Transform pitchRoot;
+    [SerializeField] private float mouseSensitivity;
+
+    private bool isGrounded;
+    private bool jumpRequested;
+    public Rigidbody rb;
+    [SerializeField] private float movespeed = 8.0f;
+
+    private float horizontal;
+    private float vertical;
+
+    private float pitch;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jumpRequested = true;
+        }
+
+        HandleLook();
+    }
+    private void FixedUpdate()
+    {
+        PlayerMove();
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (jumpRequested && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            jumpRequested = false;
+        }
+    }
+    void PlayerMove()
+    {
+        Vector3 currentVel = rb.velocity;
+        Vector3 newVel = new Vector3(horizontal * movespeed, currentVel.y, vertical * movespeed);
+        rb.velocity = Vector3.Lerp(rb.velocity, newVel, Time.deltaTime * 10.0f);
+    }
+
+    void HandleLook()
+    {
+        float mx = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float my = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        if (yawRoot) yawRoot.Rotate(Vector3.up, mx, Space.World);
+
+        pitch = Mathf.Clamp(pitch - my, -85f, 85f);
+        if (pitchRoot) pitchRoot.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
